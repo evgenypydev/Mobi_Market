36587164bf0f2eeaee82fbb20ee0566148b93ae8
Your Name <ytrokinterteiment@gmail.com>
2023-06-15 13:02:56 +0200

    Add urls for update personal info, send l and verify code

--- mobi_market/users/urls.py
+++ mobi_market/users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .endpoints import UserCreateAPIView
+from .endpoints import UserCreateAPIView, PhoneNumberVerificationAPIView, UpdateUserPersonalInfoAPIView, PhoneNumberVerifyAPIView
 
 
 urlpatterns = [
     path("api/register/", UserCreateAPIView.as_view(), name="register"),
+    path("api/update_user/", UpdateUserPersonalInfoAPIView.as_view(), name="add_personal_info"),
     path('api/token/', TokenObtainPairView.as_view(), name='jwt_create'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/phone/', PhoneNumberVerificationAPIView.as_view(), name='send_verification_code'),
+    path('api/phone/verify/',PhoneNumberVerifyAPIView.as_view(), name='phone_verification'),
 ]
