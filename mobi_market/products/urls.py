735752afc798c5d63f2c7774fe2d8c9b3d701724
Your Name <ytrokinterteiment@gmail.com>
2023-06-23 10:36:45 +0200

    Create urls

---
+++ mobi_market/products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path, re_path
+from rest_framework import routers
+
+from .endpoints import CardProductByUserViewSet, CardProductListAPIView, CardProductRetrieveAPIView
+
+router = routers.DefaultRouter()
+router.register(r'my_products', CardProductByUserViewSet, basename='my_products')
+
+urlpatterns = [
+    path('my_products/', include(router.urls)),
+    path('main_page/', CardProductListAPIView.as_view()),
+    re_path('main_page/(?P<id>.+)/', CardProductRetrieveAPIView.as_view()),
+]
