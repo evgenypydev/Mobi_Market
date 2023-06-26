baf173d9465af0bff91509a5fef5d3bc069cf67d
Your Name <ytrokinterteiment@gmail.com>
2023-06-26 08:45:38 +0200

    Add swagger realization for all endpoints

--- mobi_market/products/endpoints.py
+++ mobi_market/products/endpoints.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -8,67 +9,60 @@ from .models import CardProduct
 from .serializers import CardCreateUpdateViewProductSerializer, CardShortViewSerializer
 
 
-# class CardProductViewSet(ModelViewSet):
-#     permission_classes = [IsAuthenticated]
-#     serializer_class = CardProductSerializer
-#
-#     def get_queryset(self, request):
-#         user = request.user
-#         queryset = CardProduct.objects.filter(id__in=user.id)
-#         return queryset
-#
-#     def create(self, request, *args, **kwargs):
-#         user = request.user
-#         if user.id == request.data["user"]:
-#             serializer = CardProductSerializer(data=request.data)
-#             serializer.is_valid(raise_exception=True)
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         else:
-#             return Response({'message': 'You cannot add a product for another user'}, status=status.HTTP_403_FORBIDDEN)
-#
-#     def partial_update(self, request, *args, **kwargs):
-#         user = request.user
-#         if user.id == request.data["user"]:
-#             instance = self.get_object()
-#             serializer = CardProductSerializer(instance, data=request.data)
-#             serializer.is_valid(raise_exception=True)
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         else:
-#             return Response({'message': 'You cannot update a product for another user'}, status=status.HTTP_403_FORBIDDEN)
-#
-#     def destroy(self, request, *args, **kwargs):
-#         user = request.user
-#         instance = self.get_object()
-#         if user.id == instance.user.id:
-#             instance.delete()
-#             return Response(status=status.HTTP_204_NO_CONTENT)
-#         else:
-#             return Response({'message': 'You cannot delete a product for another user'}, status=status.HTTP_403_FORBIDDEN)
-
-
 class CardProductByUserViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = CardShortViewSerializer
 
+
+    @swagger_auto_schema(
+        operation_description="This endpoint return all user products.",
+        responses={
+            200: CardShortViewSerializer(many=True),
+            400: 'Bad Request'
+        }
+    )
     def get_queryset(self):
         user = self.request.user
         queryset = CardProduct.objects.filter(user_id=user.id)
         return queryset
 
+    @swagger_auto_schema(
+        request_body=CardCreateUpdateViewProductSerializer,
+        operation_description="This endpoint create user product.",
+        responses={
+            201: CardCreateUpdateViewProductSerializer,
+            400: 'Bad Request'
+        }
+    )
     def create(self, request, *args, **kwargs):
+        user = self.request.user
+        request.data["user_id"] = user.id
         serializer = CardCreateUpdateViewProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    @swagger_auto_schema(
+        request_body=CardCreateUpdateViewProductSerializer,
+        operation_description="This endpoint update user product.",
+        responses={
+            201: CardCreateUpdateViewProductSerializer,
+            400: 'Bad Request'
+        }
+    )
     def partial_update(self, request, *args, **kwargs):
         serializer = CardCreateUpdateViewProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(
+        operation_description="This endpoint retrieve user product.",
+        responses={
+            201: CardCreateUpdateViewProductSerializer,
+            400: 'Bad Request'
+        }
+    )
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = CardCreateUpdateViewProductSerializer(instance)
